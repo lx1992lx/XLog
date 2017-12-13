555628602a227eccafdfbbbc059ecd4acbe34b35
lx1992lx <1012025958@qq.com>
2017-12-13 14:51:26 +0800

    添加图片

--- app/src/main/java/com/yyxk/xlogtext/MainActivity.kt
+++ app/src/main/java/com/yyxk/xlogtext/MainActivity.kt
@@ -10,7 +10,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        XLog.i("on Kotlin Activity create")
+        XLog.wtf("on Kotlin Activity create")
         var intent= Intent(this,JavaActivity::class.java)
         startActivity(intent)
     }
